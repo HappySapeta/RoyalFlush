9250134ffc8e952f3f0d426557034c159c77be59
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-02-11 14:27:45 +0000

    New assets to test DlgSystem.

--- Source/RoyalFlush/Components/DialogueComponent.h
+++ Source/RoyalFlush/Components/DialogueComponent.h
@@ -2,18 +2,19 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "DlgSystem/DlgDialogueParticipant.h"
 #include "DialogueComponent.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class ROYALFLUSH_API UDialogueComponent : public UActorComponent
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ROYALFLUSH_API UDialogueComponent : public UActorComponent, public IDlgDialogueParticipant
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
 	UDialogueComponent();
-
+ 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -21,7 +22,4 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
 };
