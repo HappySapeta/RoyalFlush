0a50f430dde7cb2d3b680d52902b71672d06e2fe
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-02-18 18:59:00 +0000

    Dialogue UI and logic

---
+++ Source/RoyalFlush/UI/DialogueOptionEntryWidget.h
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2026 VINNIE BRIGHTEY, FELICITY ZABAVA, ARTHUR NORTH, JOSH BENNETTS, LEWIS TAIT, KYLE MURRAY, HOLLY ALBERT, ANUPAM SAHU, ARAMINTA MCDIARMID. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "Blueprint/UserWidget.h"
+
+#include "DialogueOptionEntryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROYALFLUSH_API UDialogueOptionEntryWidget : public UUserWidget, public IUserObjectListEntry
+{
+	GENERATED_BODY()
+};
