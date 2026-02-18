bc16238c709041f5a0316a4bdb4cd3630a97a654
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-02-18 11:32:49 +0000

    OnInteractionStarted event.

---
+++ Source/RoyalFlush/RFCharacter.h
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2026 VINNIE BRIGHTEY, FELICITY ZABAVA, ARTHUR NORTH, JOSH BENNETTS, LEWIS TAIT, KYLE MURRAY, HOLLY ALBERT, ANUPAM SAHU, ARAMINTA MCDIARMID. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RFCharacter.generated.h"
+
+UCLASS()
+class ROYALFLUSH_API ARFCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	// Sets default values for this character's properties
+	ARFCharacter();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void OnInteractionStarted(const ARFCharacter* Other);
+	
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+};
