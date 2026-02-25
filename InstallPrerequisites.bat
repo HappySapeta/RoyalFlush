5ff3536b4d3fb6e9f37b0f4a7dd8ad26ede17ee1
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-02-25 19:42:51 +0000

    Automation script to update submodules.
    1. Automatically install Visual Studio Build Tools.

--- InstallPrerequisites.bat
+++ InstallPrerequisites.bat
@@ -1,3 +1,5 @@
+winget install -e --id Microsoft.VisualStudio.BuildTools
+
 "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" modify ^
 --installPath "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools" ^
 --add Microsoft.VisualStudio.Workload.ManagedDesktopBuildTools ^
