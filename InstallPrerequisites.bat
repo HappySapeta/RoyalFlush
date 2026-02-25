88e9ade70efeb8ab48d70ba70b90dcde132d2313
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-02-25 18:03:35 +0000

    Prerequisite Installation script.

---
+++ InstallPrerequisites.bat
@@ -0,0 +1,10 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" modify ^
+--installPath "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools" ^
+--add Microsoft.VisualStudio.Workload.ManagedDesktopBuildTools ^
+--add Microsoft.VisualStudio.Workload.VCTools ^
+--add Microsoft.VisualStudio.Component.Windows11SDK.26100 ^
+--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
+--add Microsoft.VisualStudio.Component.VC.14.44.17.14.x86.x64 ^
+--add Microsoft.NetCore.Component.SDK ^
+--add Microsoft.Net.ComponentGroup.DevelopmentPrerequisites ^
+--add Microsoft.Net.ComponentGroup.4.8.DeveloperTools ^
