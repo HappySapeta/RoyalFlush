a45fc30de4a9ff91a0e2eacee47eca55ed5cd265
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-03-21 02:04:06 +0000

    Journal component
    1. Added a Journal component to GM_Default.

---
+++ Source/RoyalFlush/Journal/RFJournalComponent.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2026 VINNIE BRIGHTEY, FELICITY ZABAVA, ARTHUR NORTH, JOSH BENNETTS, LEWIS TAIT, KYLE MURRAY, HOLLY ALBERT, ANUPAM SAHU, ARAMINTA MCDIARMID. All rights reserved.
+#include "RFJournalComponent.h"
+
+URFJournalComponent::URFJournalComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+int URFJournalComponent::AddClue(const URFClueDataAsset* ClueObject)
+{
+	const FRFClue& ClueData = ClueObject->GetData();
+	if (!Clues.Contains(ClueData))
+	{
+		Clues.Add(ClueData);
+		return Clues.Num() - 1;
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Attempted to add existing clue."));
+	return -1;
+}
+
+void URFJournalComponent::StageClue(const int ClueIndex)
+{
+	if (Clues.IsValidIndex(ClueIndex))
+	{
+		StagedClues.Push(Clues[ClueIndex]);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Attempted to stage invalid clue index."));
+	}
+}
+
+void URFJournalComponent::GetStagedClues(TArray<FRFClue>& Out_StagedClues)
+{
+	Out_StagedClues = StagedClues;
+}
+
+void URFJournalComponent::Accept()
+{
+	OnCluesSubmittedEvent.Broadcast();
+}
+
+void URFJournalComponent::BindOnCluesSubmitted(FOnCluesSubmittedDelegate Callback)
+{
+	OnCluesSubmittedEvent = Callback;
+}
---
+++ Source/RoyalFlush/Journal/RFJournalComponent.h
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2026 VINNIE BRIGHTEY, FELICITY ZABAVA, ARTHUR NORTH, JOSH BENNETTS, LEWIS TAIT, KYLE MURRAY, HOLLY ALBERT, ANUPAM SAHU, ARAMINTA MCDIARMID. All rights reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RFClueDataAsset.h"
+#include "Components/ActorComponent.h"
+#include "RFJournalComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ROYALFLUSH_API URFJournalComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCluesSubmittedDelegate);
+	
+public:
+
+	// Sets default values for this component's properties
+	URFJournalComponent();
+	
+	UFUNCTION(BlueprintCallable)
+	int AddClue(const URFClueDataAsset* ClueObject);
+	
+	UFUNCTION(BlueprintCallable)
+	void StageClue(const int ClueIndex);
+	
+	UFUNCTION(BlueprintCallable)
+	void GetStagedClues(TArray<FRFClue>& Out_StagedClues);
+	
+	UFUNCTION(BlueprintCallable)
+	void Accept();
+	
+	void BindOnCluesSubmitted(FOnCluesSubmittedDelegate Callback);
+	
+protected:
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnCluesSubmittedDelegate OnCluesSubmittedEvent;
+	
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	TArray<FRFClue> Clues;
+	
+	UPROPERTY(VisibleAnywhere)
+	TArray<FRFClue> StagedClues;
+	
+};
