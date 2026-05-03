1f04aef44c21cf32b85c295c24391c9314741386
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-05-03 23:46:46 +0100

    Endings (WIP)

---
+++ Source/RoyalFlush/RFGameInstance.h
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2026 VINNIE BRIGHTEY, FELICITY ZABAVA, ARTHUR NORTH, JOSH BENNETTS, LEWIS TAIT, KYLE MURRAY, HOLLY ALBERT, ANUPAM SAHU, ARAMINTA MCDIARMID. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RFGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROYALFLUSH_API URFGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintCallable)
+	bool GetGameResult() const
+	{
+		return bDidPlayerWin;
+	}
+	
+	UFUNCTION(BlueprintCallable)
+	void SetGameResult(const bool bWin)
+	{
+		bDidPlayerWin = bWin;
+	}
+
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	bool bDidPlayerWin = false;
+	
+};
